6c668944c999d1d42a55c4b50fdf5464962d3687
Chathushi <ranasinghechathushi@gmail.com>
2024-10-25 00:35:44 +0530

    Connected to the weather API

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,50 +1,118 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 //Weather display component
 export default function WeatherCard(props){
 
-    /*let weatherData = [
-        {date:"15/10", emoji:"",temp:"20C"},
-        {date:"16/10", emoji:"",temp:"27C"},
-        {date:"17/10", emoji:"",temp:"24C"},
-        {date:"18/10", emoji:"",temp:"23C"},
-        {date:"19/10", emoji:"",temp:"20C"}
-    ]*/
+    //Initialize state variable to manage weather data
+    const[weatherData, setWeatherData] = useState({
+        temp : "",
+        icon: "",
+        description: "",
+        humidity: "",
+        wind_kph:"",
+        feelslike_c: "",
+        forecastday: [{
+            date: "",
+            avgtemp_c: "",
+            day_icon: ""
+        }]
+    })
+    
+    const[loadIcon, setLoadIcon] = useState(true)
 
-    //const url ="https://pro.openweathermap.org/data/2.5/forecast/hourly?q=sydney&appid=03e543f10ebdff21293e18845982b67f"
+
+    //add a side effect interact with the public weather api
+    useEffect(function(){
+        async function fetchWeatherData(){
+            try{
+                //fetch data from the https request
+                const response = await fetch (`https://api.weatherapi.com/v1/forecast.json?key=ef16ab0456214c64be074504241610&q=${props.city}&days=3&aqi=no&alerts=no`)
+                //parse the response in json
+                const data = await response.json()
+                //assign the retrieved data to the weatherData object
+                setWeatherData(function(prevWeatherData)  {
+                    return(
+                       { 
+                            ...prevWeatherData,
+                            temp: data.current.temp_c,
+                            icon: data.current.condition.icon,
+                            description: data.current.condition.text,
+                            humidity: data.current.humidity,
+                            wind_kph: data.current.wind_kph,
+                            feelslike_c: data.current.feelslike_c,
+
+                            forecastday: data.forecast.forecastday.map(function(dayItem){
+                                return(
+                                    {
+                                        date: dayItem.date,
+                                        avgtemp_c: dayItem.day.avgtemp_c,
+                                        day_icon: dayItem.day.condition.icon
+                                    }
+                                )
+
+                            })
+                        }
+
+                    )
+                })
+            }catch(error){
+                console.log("Error: " + error)      //log any error
+            }finally{
+                //after data is received, the loading text should not be displayed
+                setLoadIcon(false)
+            }
+        }
+        fetchWeatherData()
+    },[props.city])
+
+
+    
     return(
-        <div className="weather-display">
-            <h3 className="city-name">{props.city}</h3>
-            <p className="temp">28C</p>
-            <p className="weather-emoji">☀</p>
-            <p className="weather-desc">Clear skies</p>
-            <div className="other-details-display">
-                <div className="humidity-display">
-                    <p className="humidity-value">86%</p>
-                    <p className="other-label">Humidity</p>
+        <div>
+        {loadIcon ? (
+            <div className="loading-text">Please wait...</div>
+        ) : (
+            <div className="weather-display">
+                <h3 className="city-name">{props.city}</h3>
+                <p className="temp">{weatherData.temp}</p>
+                <img alt="weather-icon" className="weather-emoji" src={weatherData.icon}/>
+                <p className="weather-desc">{weatherData.description}</p>
+                <div className="other-details-display">
+                    <div className="humidity-display">
+                        <p className="humidity-value">{weatherData.humidity}%</p>
+                        <p className="other-label">Humidity</p>
+                    </div>
+                    <div className="wind-display">
+                        <p className="windspeed-value">{weatherData.wind_kph}kph</p>
+                        <p className="other-label">Winds</p>
+                    </div>
+                    <div className="feels-like-display">
+                        <p className="feels-like-value">{weatherData.feelslike_c}C</p>
+                        <p className="other-label">Feels Like</p>
+                    </div>
                 </div>
-                <div className="wind-display">
-                    <p className="windspeed-value">20mph</p>
-                    <p className="other-label">Winds</p>
-                </div>
-                <div className="feels-like-display">
-                    <p className="feels-like-value">86%</p>
-                    <p className="other-label">Feels Like</p>
-                </div>
-            </div>
-            <hr className="line-break"></hr>
+                <hr className="line-break"></hr>
                 <div className="show-forecast-display">
-                    {weatherData.map((weather,index) => (
-                        <div className="day-forecast">
-                            <p className="date">{weather.date}</p>
-                            <p className="day-emoji">{weather.emoji}</p>
-                            <p className="day-temp">{weather.temp}</p>
-                        </div>
-                        )) 
+                    {
+                    (weatherData.forecastday).map((day) => (
+                            <div className="day-forecast">
+                                <p className="date">{day.date}</p>
+                                <img alt="weather-day-icon" className="day-emoji" src={day.day_icon}/>
+                                <p className="day-temp">{day.avgtemp_c}</p>
+                            </div>
+                        ))
+
                     }
                 </div>
-        </div>
+           </div>
+        )}
+    </div>
         
     )
+
 }
 
+
+
+/** */
